ee269cfa18e4865345914ecdee69e300cf3c8bff
F.A. van Steen <f.a.vansteen@student.tudelft.nl>
2022-10-06 15:31:10 +0200

    Big clean up of mesh class

--- Classical RBF/src/Mesh.cpp
+++ Classical RBF/src/Mesh.cpp
@@ -21,23 +21,21 @@ void Mesh::readMeshFile(std::vector<std::string> ibTags,std::vector<std::string>
 		std::cout << "Reading mesh file: " << ifName << std::endl;
 	}
 
-	int lineNo =0;	// keep track of the line number
-	int nIntBdry = 0, nExtBdry = 0;		// ints for keeping track of number of internal/ external boundary points
-	bool intBound = false, extBound = false;	// booleans to acknowledge if on int/ext boundary
-	int cntInt = 0,cntExtNodes = 0;					// counters for int/ext boundary nodes
-	int point = 0;								// counter for number of points
+	int lineNo = 0;								// line number counter
+	int nIntBdryElems = 0, nExtBdryElems = 0;	// variables that sum the amount of marker elements
+	bool intBdry = false, extBdry = false;		// booleans to acknowledge if on int/ext boundary
+	int nIntBdryNodes = 0, nExtBdryNodes = 0;	// counters for int/ext boundary nodes
+	int nPnts = 0;								// counter for number of points
 	int pntsIdx;								// int that stores the line where "NPOIN= " is
-	int cntExtElems =0;			// counts the number of elements in the boundaries
-	Eigen::ArrayXXi testArr;
-	int testCnt = 0;
-	//TODO make following statement dynamic
-	Eigen::ArrayXi idxMovingNodes(8);	// indices of the moving nodes on the ext boundary
-//	idxMovingNodes(0) = 0;			// first element is saved as moving node
+	Eigen::ArrayXXi extBdryNodesMat(0,3);
+	Eigen::ArrayXXi intBdryNodesMat(0,3);
 
-	Eigen::ArrayXXi nodeData;	//testing
+	int extBdryElemCnt = 0;
+	int intBdryElemCnt = 0;// counting the elements of the external boundary
+	Eigen::ArrayXi extBdryEndsIdx(2*ebTags.size());
 
-	int bdryElems;				// locally stores how many elements are in that boundary
-	int cntExtBdry = 0;			// counting the number of ext boundaries
+	int MarkerElems;				// locally stores how many elements are in that boundary
+	int nExtMarker = 0; 				// Counts the number of external boundary markers
 
 	std::string line;	// string containing line obtained by getline() function
 	std::ifstream mFile(ifName); 	//opening file name stored in mFile object
@@ -46,17 +44,16 @@ void Mesh::readMeshFile(std::vector<std::string> ibTags,std::vector<std::string>
 		//Obtain line
 		while (getline(mFile, line)){
 
-			// Following statements find information on various lines of the input file
-			if (line.rfind("NDIME= ",0)==0){
+			if (line.rfind("NDIME= ",0)==0){							// save number of dimensions
 				nDims = stoi(line.substr(7));
 			}
-			else if (line.rfind("NPOIN= ",0)==0){
+			else if (line.rfind("NPOIN= ",0)==0){						// save nr of points
 				nNodes = stoi(line.substr(7));
-				pntsIdx = lineNo;
-				coords.resize(nNodes, nDims);
+				pntsIdx = lineNo;										// save line nr.
+				coords.resize(nNodes, nDims);							// resizing the array containing the coordinates
 				std::cout << "Saving node coordinates" << std::endl;
 			}
-			else if (line.rfind("NELEM= ",0)==0){
+			else if (line.rfind("NELEM= ",0)==0){						// save nr of elements
 				nElem = stoi(line.substr(7));
 			}
 
@@ -85,16 +82,16 @@ void Mesh::readMeshFile(std::vector<std::string> ibTags,std::vector<std::string>
 						if(lvl >=2){
 							std::cout << "Saving nodes of internal boundary: " << tag << std::endl;
 						}
-						intBound = true;
-						extBound = false;
+						intBdry = true;
+						extBdry = false;
 					}
 					// Check if found tag is among provided external boundary tags
 					else if(std::find(std::begin(ebTags), std::end(ebTags), tag) != std::end(ebTags)){
 						if(lvl >= 2){
 							std::cout << "Saving nodes of external boundary: " << tag << std::endl;
 						}
-						intBound = false;
-						extBound = true;
+						intBdry = false;
+						extBdry = true;
 					} else throw(tag);
 				}
 
@@ -105,143 +102,176 @@ void Mesh::readMeshFile(std::vector<std::string> ibTags,std::vector<std::string>
 			}
 
 			// Check whether line corresponds to internal boundary
-			else if(intBound){
+			else if(intBdry){
 				// check if line states the number of elements in the boundary
 				if(line.rfind("MARKER_ELEMS= ",0)==0){
-					nIntBdry += stoi(line.substr(13));	// updating number of int. boundary elements
-					intBdryNodes.conservativeResize(4*nIntBdry);	// resizing the array containing the int. boundary nodes
+					nIntBdryElems += stoi(line.substr(13));	// updating number of int. boundary elements
+					intBdryNodesMat.conservativeResize(nIntBdryElems,intBdryNodesMat.cols());
 
 				}
 				// check if line contains nodes corresponding to the boundary
 				else if(isdigit(line[0])){
-					std::istringstream is(line.substr(1)); 	// split line at '\t' and omitting the first number
-					int node;
-					while(is >> node){					// While the stringstream contains nodes
-						intBdryNodes[cntInt]  = node;	// include node in intBdryNodes
-						cntInt++;						// update counter
+					std::istringstream is(line.substr(0)); 	// split line at '\t' and omitting the first number
+					int data;
+					int lineElem = 0;
+					while(is >> data){
+						if(lineElem == 0 && data == 5 && intBdryNodesMat.cols() < 4){
+							intBdryNodesMat.conservativeResize(nIntBdryElems,4);
+						}
+						else if(lineElem == 0 && data == 9 && intBdryNodesMat.cols() < 5){
+							intBdryNodesMat.conservativeResize(nIntBdryElems,5);
+						}
+						intBdryNodesMat(intBdryElemCnt,lineElem) = data;
+						if(lineElem>0){
+							nIntBdryNodes++;
+						}
+						lineElem++;
 					}
+					intBdryElemCnt++;
 				}
 			}
 
 			// Check whether line corresponds to ext. boundary
-			else if(extBound){
+			else if(extBdry){
 				// Check if line states number of elements in boundary
 				if(line.rfind("MARKER_ELEMS= ",0)==0){
-
-					// If there are more than 2 ext boundaries than the first and last index is saved
-					if(cntExtBdry>0 && cntExtBdry<int(ebTags.size())){
-						idxMovingNodes(2*cntExtBdry-1) = cntExtNodes-1;
-						idxMovingNodes(2*cntExtBdry) = cntExtNodes;
-					}
-
-					bdryElems = stoi(line.substr(13));
-					nExtBdry += bdryElems;								// updating number of external boundary elements
-//					extBdryNodes.conservativeResize(8*nExtBdry); 		// resizing the array containing ext. boundary nodes
-					nVecs.conservativeResize(nExtBdry,nDims);
-					tVecs.conservativeResize(nExtBdry,nDims);
-					midPnts.conservativeResize(nExtBdry,nDims);
-					nodeData.conservativeResize(nExtBdry,4);
-					nodeData(Eigen::lastN(bdryElems),Eigen::all) = Eigen::ArrayXXi::Zero(bdryElems,4);
-//					<< Eigen::ArrayXXi::Zero(nExtBdry,5);
-
-					std::cout << nodeData << std::endl;
-					cntExtBdry++;
-
-					testArr.conservativeResize(nExtBdry,8);
-
+					MarkerElems = stoi(line.substr(13));
+					nExtBdryElems+= MarkerElems;
+					extBdryEndsIdx(nExtMarker) = nExtBdryElems-1;
+					extBdryNodesMat.conservativeResize(nExtBdryElems,extBdryNodesMat.cols());
+					nExtMarker++;
 				}
 				else if(isdigit(line[0])){
-//					getExtBdryData(line, cntExtElems, cntExtNodes,ebTags.size(),bdryElems,nodeData);
 					std::istringstream is(line);
 					int data;
-					int nodeCnt = 0;
+					int lineElem = 0;
 					while(is >> data){
-						testArr(testCnt,nodeCnt) = data;
-						nodeCnt++;
+						if(lineElem == 0 && data == 5 && extBdryNodesMat.cols() < 4){
+							extBdryNodesMat.conservativeResize(nExtBdryElems,4);
+						}
+						else if(lineElem == 0 && data == 9 && extBdryNodesMat.cols() < 5){
+							extBdryNodesMat.conservativeResize(nExtBdryElems,5);
+						}
+						extBdryNodesMat(extBdryElemCnt,lineElem) = data;
+						if(lineElem>0){
+							nExtBdryNodes++;
+						}
+						lineElem++;
 					}
-					testCnt++;
-
-
+					extBdryElemCnt++;
 				}
 			}
 
 			// Check if line corresponds to line containing node coordinates
-			if (lineNo > pntsIdx && point < nNodes){
+			if (lineNo > pntsIdx && nPnts < nNodes){
 				std::istringstream is(line);
 				switch(nDims){
 					case 2:
-						is >> coords(point,0) >> coords(point,1);
+						is >> coords(nPnts,0) >> coords(nPnts,1);
 						break;
 					case 3:
-						is >> coords(point,0) >> coords(point,1) >> coords(point,2);
+						is >> coords(nPnts,0) >> coords(nPnts,1) >> coords(nPnts,2);
 						break;
 				}
-				point++;
+				nPnts++;
 			}
 			lineNo++;
 		}
 		mFile.close();
 	}
 	else std::cout << "Not able to open input mesh file";
-	//
-	std::cout << testArr << std::endl;
-	std::exit(0);
-//	std::cout << nodeData << std::endl;
 
-	int colCnt = 0;
-	while(colCnt < nodeData.cols()){
-		if(nodeData.col(colCnt).sum() ==0){
-			break;
-		}
-		colCnt++;
-	}
 
-	/* Made a better way to collect the boundary nodes. This is done by saving each element on a row of nodeData.
-	 * This array is then passed to getNormalsTest in order to obtain the midPnts and vectors.
-	 * The array with unique boundary nodes is obtained by conconating the columns of nodeData.
-	 * followed by the respective sorting and removing duplicate elements.
-	 * Same operation can be performed for the internal nodes.
-	 *
-	 * When the idx of the movingNodes are saved then the corresponding rows of nodeData can be accessed.
-	 * the idx is the cumulative sum of the number of elements per external boundary.
-	 * from the resulting rows the duplicate nodes should be saved since these are present on both boundaries and therfore
-	 * are moving nodes with zero displacement
-	 *
-	 */
+	getBdryNodes(intBdryNodesMat, intBdryNodes, nIntBdryNodes, nIntBdryElems);
+	getBdryNodes(extBdryNodesMat, extBdryNodes, nExtBdryNodes, nExtBdryElems);
 
-	nodeData.conservativeResize(nodeData.rows(),colCnt);
+	getMovingNodes(ebTags,extBdryNodesMat,extBdryEndsIdx);
 
-	midPntsT.resize(nExtBdry,nDims);
-	nVecsT.resize(nExtBdry,nDims);
-	tVecsT.resize(nExtBdry,nDims);
-	getNormalsTest(nodeData);
+	getSlidingNodes();
+	std::cout << "Internal boundary nodes: \n" << intBdryNodes <<std::endl;
+	std::cout << "External boundary nodes: \n" << extBdryNodes <<std::endl;
+	std::cout << "Moving boundary nodes: \n" << movingNodes <<std::endl;
+	std::cout << "Sliding boundary nodes: \n" << slidingNodes << std::endl;
 
-	std::cout << nodeData << std::endl;
 
-	extBdryNodes.resize(nodeData.rows()*colCnt);
-	extBdryNodes << nodeData.col(0),nodeData.col(1);
-	extBdryNodes = removeDuplicates(extBdryNodes);
-	std::cout << extBdryNodes << std::endl;
-		//
-//	idxMovingNodes.tail(1) = cntExtNodes-1;
-//	std::cout << idxMovingNodes<< std::endl;
 
-//	movingNodes = extBdryNodes(idxMovingNodes);
-//	movingNodes = removeDuplicates(movingNodes);
-	intBdryNodes.conservativeResize(cntInt-1);
-	intBdryNodes = removeDuplicates(intBdryNodes);
 
-//	extBdryNodes.conservativeResize(cntExtNodes-1);
-//	extBdryNodes = removeDuplicates(extBdryNodes);
+//
+//	midPntsT.resize(nExtBdryElems,nDims);
+//	nVecsT.resize(nExtBdryElems,nDims);
+//	tVecsT.resize(nExtBdryElems,nDims);
+//	getNormalsTest(nodeData);
 
-	bdryNodes.resize(intBdryNodes.size()+extBdryNodes.size());
-	bdryNodes << intBdryNodes, extBdryNodes;
+	getIntNodes();
 
-	obtainIntNodes();
+	std::cout <<"Internal nodes: \n"<< intNodes << std::endl;
 	//TODO if there is only a single element then movingNodes does not exist
-	slidingNodes = uniqueElems(movingNodes, extBdryNodes);
+
 //	slidingNodes = extBdryNodes;
 	std::cout << "Mesh file read successfully" << std::endl;
+}
+
+void Mesh::getBdryNodes(Eigen::ArrayXXi bdryNodesMat, Eigen::ArrayXi& bdryNodesArr, int nBdryNodes, int nBdryElems){
+	bdryNodesArr.resize(nBdryNodes);
+	int count = 0;
+	for(int i = 0; i < nBdryElems; i++){
+		int n;
+		switch (bdryNodesMat(i,0)){
+			case 3:
+				n = 2;
+				break;
+			case 5:
+				n = 3;
+				break;
+		}
+		int j = 0;
+		while(j<n){
+			bdryNodesArr(count) = bdryNodesMat(i,j+1);
+			j++;count++;
+		}
+	}
+	bdryNodesArr = removeDuplicates(bdryNodesArr);
+}
+
+void Mesh::getMovingNodes(std::vector<std::string> ebTags, Eigen::ArrayXXi extBdryNodesMat, Eigen::ArrayXi extBdryEndsIdx){
+
+	// Adding first element indices
+	for(int i = 0; i < int(ebTags.size())-1; i ++){
+		extBdryEndsIdx(ebTags.size()+i) = extBdryEndsIdx(i)+1;
+	}
+	extBdryEndsIdx.tail(1) = 0;
+
+	Eigen::ArrayXi extBdryEnds(extBdryEndsIdx.size()*(extBdryNodesMat.cols()-1));
+//	movingNodes.resize(extBdryEndsIdx.size()*(extBdryNodesMat.cols()-1));
+
+	int idx = 0;
+	for(int i = 0; i < extBdryEndsIdx.rows(); i++){
+		switch(extBdryNodesMat(extBdryEndsIdx(i),0)){
+			case 3:
+				extBdryEnds(Eigen::seqN(idx,2)) = extBdryNodesMat(extBdryEndsIdx(i),Eigen::seqN(1,2));
+
+				idx = idx+2;
+				break;
+			case 5:
+				extBdryEnds(Eigen::seqN(idx,3)) = extBdryNodesMat(extBdryEndsIdx(i),Eigen::seqN(1,3));
+				idx = idx+3;
+				break;
+		}
+
+	}
+	extBdryEnds.conservativeResize(idx+1);
+	std::sort(std::begin(extBdryEnds), std::end(extBdryEnds));
+
+	movingNodes.conservativeResize(idx+1);
+	int movingNodeIdx = 0;
+	for(int i = 0; i<idx; i++){
+		if(extBdryEnds(i+1) == extBdryEnds(i)){
+			movingNodes(movingNodeIdx)=  extBdryEnds(i+1);
+			movingNodeIdx++;
+		}
+	}
+
+	movingNodes.conservativeResize(movingNodeIdx);
 }
 
 Eigen::ArrayXi Mesh::removeDuplicates(Eigen::ArrayXi& arr){
@@ -271,47 +301,42 @@ Eigen::ArrayXi Mesh::removeDuplicates(Eigen::ArrayXi& arr){
 	return uniqueElmnts(Eigen::seq(0,cnt));
 }
 
-Eigen::ArrayXi Mesh::uniqueElems(Eigen::ArrayXi& arr1, Eigen::ArrayXi& arr2){
-	/* arr1 is small array
-	 * arr2 is large array containing arr1
-	 */
-	std::cout << "Determining mutually exclusive indices" << std::endl;
-	Eigen::ArrayXi nodes(arr2.size());
-//	sort(begin(bdryNodes), end(bdryNodes));		// bdryNodes need to be sorted for this algorithm to work
-//	intNodes.resize(nNodes-bdryNodes.size());	// Resizing intNodes accordingly
+void Mesh::getSlidingNodes(){
+	std::cout << "Determining sliding nodes" << std::endl;
+	slidingNodes.resize(extBdryNodes.size()-movingNodes.size());
 
-	int cnt = 0; int i=0, j=0;					// cnt keeps track of index of intNodes, i loops over all nodes, j over the boundary nodes
+	int cnt = 0; int i=0, j=0;
 
-	while(j < int(arr2.size())){							// while loop over all nodes
-		if(i < int(arr1.size())){				// check if j is within the size of the boundary nodes
+	while(j < int(extBdryNodes.size())){							// while loop over all nodes
+		if(i < int(movingNodes.size())){				// check if j is within the size of the boundary nodes
 
-			if(arr2(j) == arr1(i)){				// if node i is equal to the j-th boundary elements then i is not a internal node
+			if(extBdryNodes(j) == movingNodes(i)){				// if node i is equal to the j-th boundary elements then i is not a internal node
 				i++; j++;						// going to next node i and element j of the boundary nodes
 			}
-			else if(arr2(j) < arr1(i)){			// if i is smaller than the j-th element of the boundary nodes
-				nodes(cnt) = arr2(j);				// then i is an internal node
+			else if(extBdryNodes(j) < movingNodes(i)){			// if i is smaller than the j-th element of the boundary nodes
+				slidingNodes(cnt) = extBdryNodes(j);				// then i is an internal node
 				j++; cnt++;						// update index of internal nodes and i
 
 			}
-			else if(arr2(j) > arr1(i)){			// if i is larger than the j-th boundary element
+			else if(extBdryNodes(j) > movingNodes(i)){			// if i is larger than the j-th boundary element
 				i++;							// then next boundary element should be checked.
 			}
 		}
 		else{									// if all j boundary elements are checked and i < nNodes
-			nodes(cnt) = arr2(j);					// include remaining points as internal nodes
+			slidingNodes(cnt) = extBdryNodes(j);					// include remaining points as internal nodes
 			cnt++; j++;							// update index and i
 		}
 
 	}
-	return nodes(Eigen::seq(0,cnt-1));
 }
 
-void Mesh::obtainIntNodes(){
+void Mesh::getIntNodes(){
 	/* This function takes the previously found boundary nodes and compares them to an integer array containing
 	 * all nodes from 0 to nNodes. If a node is not among the boundary nodes then the node is included in
 	 * the intNodes.
 	 */
-
+	bdryNodes.resize(intBdryNodes.size()+extBdryNodes.size());
+	bdryNodes << intBdryNodes, extBdryNodes;
 	std::cout << "Determining internal nodes " << std::endl;
 
 	std::sort(std::begin(bdryNodes), std::end(bdryNodes));		// bdryNodes need to be sorted for this algorithm to work
@@ -417,46 +442,6 @@ void Mesh::getNormalsTest(Eigen::ArrayXXi nodes){
 	}
 }
 
-void Mesh::getExtBdryData(std::string& line, int& cntExtElems, int& cntExtNodes, int nBdry, int bdryElems, Eigen::ArrayXXi& nodeData){
-
-
-	std::istringstream is(line.substr(0)); 	// split line at '\t' and omitting the first number
-	int data;
-	Eigen::VectorXi dataVec(4);
-	int elemCnt = 0;
-	int nNodes;
-	while(is >> data){
-		if(elemCnt == 0){				// First element indicates the element type
-			switch(data){
-				case 3:
-					nNodes = 2;
-					break;
-				case 5:
-					nNodes = 3;
-					break;
-				case 9:
-					nNodes = 4;
-					break;
-			}
-		}
-		else{
-			dataVec(elemCnt-1) = data;	// save the remaining nodes
-		}
-		elemCnt++;
-	}
-//	std::cout << std::endl;
-//	std::cout << cntExtElems << std::endl;
-//	std::cout << dataVec << std::endl;
-
-
-	nodeData(cntExtElems,Eigen::seq(0,nNodes-1)) = dataVec(Eigen::seq(0,nNodes-1));
-
-//	getNormals(dataVec(Eigen::seq(0,nNodes-1)),cntExtElems);
-//	extBdryNodes(Eigen::seq(cntExtNodes,cntExtNodes+nNodes-1)) = dataVec(Eigen::seq(0,nNodes-1));
-	cntExtNodes+= nNodes;
-	cntExtElems++;
-
-}
 
 void Mesh::getNodeVecs(Eigen::ArrayXi& idxs, Eigen::ArrayXXd& n, Eigen::ArrayXXd& t){
 	int idx, idx2;
--- Classical RBF/src/Mesh.h
+++ Classical RBF/src/Mesh.h
@@ -25,16 +25,16 @@ public:
 	double charLength();
 
 	Eigen::ArrayXi removeDuplicates(Eigen::ArrayXi& arr);
-	void obtainIntNodes();
+	void getIntNodes();
 	void readMeshFile(std::vector<std::string> ibTags,std::vector<std::string> ebTags,double rFac);
-	void updateNodes(Eigen::VectorXd dxVec,Eigen::VectorXd dyVec, Eigen::VectorXd xDisp,Eigen::VectorXd yDisp);
+//	void updateNodes(Eigen::VectorXd dxVec,Eigen::VectorXd dyVec, Eigen::VectorXd xDisp,Eigen::VectorXd yDisp);
 	void writeMeshFile(Eigen::MatrixXd& newCoords);
 	void getNormals(Eigen::VectorXi nodes, int& cntExtElems);
-	void getExtBdryData(std::string& line, int& cntExtElems, int& cntExtNodes, int nBdry, int bdryElems, Eigen::ArrayXXi& nodeData);
 	void getNodeVecs(Eigen::ArrayXi& idxs, Eigen::ArrayXXd& n, Eigen::ArrayXXd& t);
-	Eigen::ArrayXi uniqueElems(Eigen::ArrayXi& arr1, Eigen::ArrayXi& arr2);
+	void getSlidingNodes();
 	void getNormalsTest(Eigen::ArrayXXi nodes);
-
+	void getMovingNodes(std::vector<std::string> ebTags, Eigen::ArrayXXi extBdryNodesMat, Eigen::ArrayXi extBdryEndsIdx);
+	void getBdryNodes(Eigen::ArrayXXi bdryNodesMat, Eigen::ArrayXi& bdryNodesArr, int nBdryNodes, int nBdryElems);
 };
 
 #endif /* MESH_H_ */
